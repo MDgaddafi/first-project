5a18073b15c58d35bc9f6788200fc7c9febd0688
Babua Hossain <babuahossain@Babuas-Mac-Studio.local>
2024-11-28 04:35:32 +0600

    Done Create student feature

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { StudentRouters } from './app/modules/student/student.route';
 
 // parsers
 app.use(express.json());
 app.use(cors());
+// application routes
+app.use('/api/v1/students', StudentRouters);
 
-app.get('/', (req: Request, res: Response) => {
+const getAController = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
-});
+};
+app.get('/', getAController);
 
 export default app;
